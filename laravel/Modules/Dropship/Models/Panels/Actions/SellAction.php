c140a748627878d09ec196232315c91d8f079b40
Davide <davidecavallini1987@gmail.com>
2021-04-04 11:56:44 +0200

    up

--- laravel/Modules/Dropship/Models/Panels/Actions/SellAction.php
+++ laravel/Modules/Dropship/Models/Panels/Actions/SellAction.php
@@ -7,18 +7,16 @@ use Modules\Dropship\Models\Product;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 
 //-------- bases -----------
-class ItemAction extends XotBasePanelAction {
+class SellAction extends XotBasePanelAction
+{
 
     public bool $onItem = true;
-    public string $icon = '<i class="fas fa-upload"></i>';
+    public string $icon = '<i class="fas fa-check-circle"></i>';
 
-    public function handle() {
-        $this->row->published=true;
+    public function handle()
+    {
+        $this->row->published = true;
         $this->row->save();
         return redirect()->back();
-
     }
-
-
-
 }
--- laravel/Modules/Dropship/Models/Product.php
+++ laravel/Modules/Dropship/Models/Product.php
@@ -5,23 +5,25 @@ namespace Modules\Dropship\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Product extends BaseModelLang {
+class Product extends BaseModelLang
+{
 
     /**
      * @var string[]
      */
-    protected $fillable = ['id', 'url', 'title', 'description', 'img_url',
+    protected $fillable = [
+        'id', 'url', 'title', 'description', 'img_url',
         'purchase_price', 'purchase_vat_percentable', 'purchase_taxable_price', 'purchase_vat_price',
         'shipment_price', 'shipment_vat_percentable', 'shipment_taxable_price', 'shipment_vat_price',
         'selling_price', 'selling_vat_percentable', 'selling_taxable_price', 'selling_vat_price',
         'total_sales_commission', 'total_revenue', 'vat_tax_autority', 'personal_income_tax', 'net_income',
-        'shareholders_number', 'dividend','published'];
+        'shareholders_number', 'dividend', 'published', 'sold'
+    ];
     protected $appends = ['stock'];
 
-    public function getStockAttribute() {
+    public function getStockAttribute()
+    {
         //qua va fatto il parsing dell'url
         return 999;
     }
-
-
 }
